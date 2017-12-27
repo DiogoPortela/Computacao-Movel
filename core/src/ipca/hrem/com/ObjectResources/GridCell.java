3e427c18352016369931c7c0cbf4d6456c1177f8
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2017-12-27 00:40:32 +0000

    Build Input beginning
    -Changed TileTexture to one file.
    -Added buttons to ButtonSheet.
    -Added getsetters for Map class.
    -Added click reactions to BuildInput.
    -Changed Background color to black.
    -Added some randomness for grass.
    -Add new button constroctor.
    -Added buttons to Buildstate

--- core/src/ipca/hrem/com/ObjectResources/GridCell.java
+++ core/src/ipca/hrem/com/ObjectResources/GridCell.java
@@ -2,17 +2,21 @@ package ipca.hrem.com.ObjectResources;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
 
 import ipca.hrem.com.ResourceManagers.TextureManager;
 
-public class GridCell extends GameObject{
+public class GridCell extends GameObject {
 
     public enum CellType {                      //ENUMERATOR FOR ALL CELL TYPES: THIS IS SUPPOSED TO GENERATE THE SPRITE AND STUFF.
-        empty, test, selectedTest, grass, concrete, woodenFloor     //....
+        empty, test, grass, dirt, concrete, woodenFloor     //....
     }
 
     //-------------------------Variables-------------------------//
+    public static final String tileTexture = "TileTexture.png";
     private Vector2 position;
     private float scale;
     private Sprite sprite;
@@ -24,6 +28,7 @@ public class GridCell extends GameObject{
     public CellType getCellType() {
         return cellType;
     }
+
     public void setCellType(CellType cellType) {
         this.cellType = cellType;
 
@@ -31,44 +36,51 @@ public class GridCell extends GameObject{
             case empty:
                 break;
             case test:
-                sprite = new Sprite(TextureManager.loadTexture("Tile001.png"));
+                sprite = new Sprite(new TextureRegion(TextureManager.loadTexture(tileTexture), 0, 0, 32, 32));
                 break;
-            case selectedTest:
-                sprite = new Sprite(TextureManager.loadTexture("Tile002.png"));
             case grass:
-                //SET SPRITE AND STATS
+                Random random = new Random();
+                sprite = new Sprite(new TextureRegion(TextureManager.loadTexture(tileTexture), 0, random.nextInt(3) * 32, 32, 32));
+                setIsInterior(false);
+                break;
+            case dirt:
+                sprite = new Sprite(new TextureRegion(TextureManager.loadTexture(tileTexture), 0, 128 * 32, 32, 32));
                 break;
         }
+        sprite.setPosition(position.x, position.y);
+        sprite.setSize(scale, scale);
     }
+
     public boolean getIsInterior() {
         return isInterior;
     }
+
     public void setIsInterior(boolean interior) {
         isInterior = interior;
     }
 
     //-------------------------Constructor-------------------------//
     public GridCell(CellType cellType, Vector2 position, float scale) {
-        setCellType(cellType);
         this.position = position;
         this.scale = scale;
-        this.sprite.setPosition(position.x * scale, position.y * scale);
-        this.sprite.setSize(scale, scale);
+        setCellType(cellType);
     }
+    //-------------------------Functions-------------------------//
 
     @Override
     public void act(Object object) {
 
     }
+
     @Override
     public void update(float deltaTime) {
 
     }
 
-    //-------------------------Functions-------------------------//
     public void render(SpriteBatch batch) {
         sprite.draw(batch);
     }
+
     public void dispose() {
         sprite.setTexture(null);
         position = null;
--- core/src/ipca/hrem/com/States/BuildState.java
+++ core/src/ipca/hrem/com/States/BuildState.java
@@ -4,26 +4,50 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import ipca.hrem.com.InputManagers.BuildInput;
 import ipca.hrem.com.MainGame;
+import ipca.hrem.com.ObjectResources.GridCell;
 import ipca.hrem.com.ObjectResources.UIResources.Button;
 
 
 public class BuildState extends GameState {
     //-------------------------Variables-------------------------//
     private Button returnToLiveBtn;
-    private Sprite tile1;
+    private Button buildBtn;
+    private Button renovateBtn;
+    private Button decorateBtn;
+
+    private Button tile1;
     //-------------------------GetSetters-------------------------//
 
     //-------------------------Constructor-------------------------//
     public BuildState() {
         setCurrentMenuSize(5.0f);
-        returnToLiveBtn = new Button(new Vector2(getCurrentMenuSizeWorld() - 2.5f, gameScaleHeight - 1.1f), new Vector2(2, 1), new Vector2(64, 32 * 3), new Vector2(64, 32)) {
+        returnToLiveBtn = new Button(new Vector2( 0.5f, gameScaleHeight - 1.1f), new Vector2(1, 1), new Vector2(128, 96), new Vector2(32, 32)) {
             @Override
             public void onClick() {
                 MainGame.setCurrentState(new LiveState());
             }
         };
         addUIObject(returnToLiveBtn);
+
+        buildBtn = new Button(new Vector2( 2.0f, gameScaleHeight - 1.1f), new Vector2(1, 1), new Vector2(160, 0), new Vector2(32, 32)) {
+            @Override
+            public void onClick() {
+                ((BuildInput) MainGame.getInputManager()).setBuildingWalls(true);
+            }
+        };
+        addUIObject(buildBtn);
+
+
+        tile1 = new Button(GridCell.tileTexture, new Vector2(0,0), new Vector2(1,1), new Vector2(0,0), new Vector2(32, 32)) {
+            @Override
+            public void onClick() {
+                setSelectedObject(this);
+                ((BuildInput)MainGame.getInputManager()).setCurrentSelectedCellType(GridCell.CellType.grass);
+            }
+        };
+        addUIObject(tile1);
     }
 
     //-------------------------Functions-------------------------//
@@ -35,6 +59,8 @@ public class BuildState extends GameState {
     @Override
     protected void renderMenu(SpriteBatch batch) {
         returnToLiveBtn.render(batch);
+        buildBtn.render(batch);
+        tile1.render(batch);
     }
 
     @Override
