45f964c07586815bfed14e97e22773ff9675fcd3
Bruno Couto <couto-10@hotmail.com>
2017-12-28 04:35:29 +0000

    Pathfinding code
    Added Node
    Added GraphGrid
    Added Pathfinding
    
    Certas partes do código tenho de ver como se põem em java (tava a fazer como se fosse c#)
    Usar ArrayList or List é igual??, tenho um problema ao usar a List.
    
    How the fuck do I create branches, tentei mas fica no mesmo... the fuck

---
+++ core/src/ipca/hrem/com/BasicResources/GraphGrid.java
@@ -0,0 +1,75 @@
+package ipca.hrem.com.BasicResources;
+
+// Represent a grid of nodes we can search paths on.
+// A 2D grid of nodes we pathfind
+// The grid marks which tiles are walkable and which are not
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class GraphGrid {
+    Node[][] nodes = new Node[1][];
+
+    int gridSizeX, gridSizeY;
+
+    // Create a new grid with tile prices
+    // width - graphgrid width
+    // height - graphgrid height
+    // tiles_costs - a 2d array, matching width and height, of tile prices.
+    // 0.0f = Unwalkable tile || 1.0f = Walkable normal tile
+    // > 1.0f = costly tile || < 1.0f = cheap tile
+    // public GraphGrid(int width, int height, float[][] tiles_costs ){
+    //     gridSizeX = width;
+    //     gridSizeY = height;
+    //     nodes = new Node[width][height];
+    //
+    // for (int x = 0; x < width; x++){
+    //         for (int y = 0; y <height; y++)
+    //        {
+    //             nodes[x, y] = new Node(tiles_costs[x, y], x, y);
+    //         }
+    //     }
+    // }
+
+    // Create a new graphgrid without tile prices, just walkable and unwalkable tiles
+    // width - graphgrid width
+    // height - graphgrid height
+    // tiles_costs - a 2d array, matching width and height, which tiles are walkable and which are not
+    public GraphGrid(int width, int height, boolean[][] walkable_tiles){
+        gridSizeX = width;
+        gridSizeY = height;
+        nodes = new Node[width][height];
+
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                nodes[x][y] = new Node(walkable_tiles[x, y] ? 1 : 0, x, y);
+            }
+        }
+    }
+
+    // Get all the neighbors of a given tile in the grid
+    // node - Node to get neighbors for
+    public List<Node> GetNeighbors(Node node){
+        List<Node> neighbors = new List<Node>();
+
+        for (int x = -1; x <= 1; x++){
+            for (int y = -1; y <= 1; y++){
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbors.add(nodes[checkX][checkY]);
+                }
+            }
+        }
+
+        return neighbors;
+    }
+
+}
---
+++ core/src/ipca/hrem/com/BasicResources/Node.java
@@ -0,0 +1,46 @@
+package ipca.hrem.com.BasicResources;
+
+//A single node in the Pathfinding grid
+
+public class Node {
+
+    // Walkable node???
+    public boolean walkable;
+    public int gridX;
+    public int gridY;
+    public float price;
+
+    // calculated values while finding path
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    // Create the grid node
+    // Price - Price to walk on this node (set to 1.0f to ignore)
+    // _gridX - Node x
+    // _gridY - Node y
+    // public Node(float _price, int _gridX, int _gridY)
+    // {
+    //    walkable = _price != 0.0f;
+    //    price = _price;
+    //    gridX = _gridX;
+    //    gridY = _gridY;
+    // }
+
+    // Create the grid node
+    // _walkable - Is this tile walkable?
+    // _gridX - Node x
+    // _gridY - Node y
+    public Node(boolean _walkable, int _gridX, int _gridY)
+    {
+        walkable = _walkable;
+        price = _walkable ? 1f : 0f;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    // Get fCost of this node
+    public int fCost() {
+        return gCost + hCost;
+    }
+}
